13d149db6c8a7a0f841ee4cb5eb7edfe2d349286
bhargav0255 <99163827+Bhargav02550@users.noreply.github.com>
2024-02-17 22:19:40 +0530

    Time: 1172 ms (13.55%) | Memory: 11.8 MB (20.33%) - LeetSync

---
+++ 79-word-search/word-search.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void search(int &c,vector<vector<char>>& board, string word,int i, int j, int n, int m,vector<vector<int>>&vis,int ind){
+        if(ind == word.size()){
+            c=1;
+            return;
+        }
+        if ((i > n - 1) || (j > m - 1) || (j < 0) || (i < 0) || (board[i][j] != word[ind]))
+            return;
+        if(!vis[i][j]){
+            vis[i][j]=1;
+            search(c,board,word,i,j+1,n,m,vis,ind+1);
+            vis[i][j]=0;
+        }
+        if(!vis[i][j]){
+            vis[i][j]=1;
+            search(c,board,word,i+1,j,n,m,vis,ind+1);
+            vis[i][j]=0;
+        }
+        if(!vis[i][j]){
+            vis[i][j]=1;
+            search(c,board,word,i,j-1,n,m,vis,ind+1);
+            vis[i][j]=0;
+        }
+        if(!vis[i][j]){
+            vis[i][j]=1;
+            search(c,board,word,i-1,j,n,m,vis,ind+1);
+            vis[i][j]=0;
+        }
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size();
+        int m = board[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        string s="";
+        int c=0;
+        //cout<<n<<" "<<m;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                search(c,board,word,i,j,n,m,vis,0);
+            }
+        }
+        cout<<c<<" ";
+        return c;
+    }
+};
