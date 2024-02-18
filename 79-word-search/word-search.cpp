087b80dde69aea07f919fffa453b6e5c4569a1d0
bhargav0255 <99163827+Bhargav02550@users.noreply.github.com>
2024-02-18 12:07:20 +0530

    Time: 1058 ms (17.75%) | Memory: 11.8 MB (20.69%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    bool search(int &c,vector<vector<char>>& board, string word,int i, int j,vector<vector<int>>&vis,int ind){
+    bool search(vector<vector<char>>& board, string word,int i, int j,vector<vector<int>>&vis,int ind){
         if(ind == word.size()){
             return true;
         }
         int n = board.size();
         int m = board[0].size();
-        if(i>=n or j>=m or j<0 or i<0 or vis[i][j]!=0 or board[i][j]!=word[ind] or c)return false;
+        if(i>=n or j>=m or j<0 or i<0 or vis[i][j]!=0 or board[i][j]!=word[ind])return false;
         vis[i][j]=1;
-        bool found=search(c,board,word,i,j+1,vis,ind+1) or
-            search(c,board,word,i+1,j,vis,ind+1) or
-            search(c,board,word,i,j-1,vis,ind+1) or
-            search(c,board,word,i-1,j,vis,ind+1);
+        bool found=search(board,word,i,j+1,vis,ind+1) or
+            search(board,word,i+1,j,vis,ind+1) or
+            search(board,word,i,j-1,vis,ind+1) or
+            search(board,word,i-1,j,vis,ind+1);
         vis[i][j]=0;
         return found;
     }
@@ -24,7 +24,7 @@ public:
         //cout<<n<<" "<<m;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(search(c,board,word,i,j,vis,0)){
+                if(search(board,word,i,j,vis,0)){
                     return true;
                 }
             }
