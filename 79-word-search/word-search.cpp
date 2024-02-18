ee4da357b939fded6bf6507a78ef4e181ecf3984
bhargav0255 <99163827+Bhargav02550@users.noreply.github.com>
2024-02-18 12:06:39 +0530

    Time: 1156 ms (14.10%) | Memory: 11.9 MB (7.24%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -9,9 +9,9 @@ public:
         if(i>=n or j>=m or j<0 or i<0 or vis[i][j]!=0 or board[i][j]!=word[ind] or c)return false;
         vis[i][j]=1;
         bool found=search(c,board,word,i,j+1,vis,ind+1) or
-        search(c,board,word,i+1,j,vis,ind+1) or
-        search(c,board,word,i,j-1,vis,ind+1) or
-        search(c,board,word,i-1,j,vis,ind+1);
+            search(c,board,word,i+1,j,vis,ind+1) or
+            search(c,board,word,i,j-1,vis,ind+1) or
+            search(c,board,word,i-1,j,vis,ind+1);
         vis[i][j]=0;
         return found;
     }
@@ -24,10 +24,8 @@ public:
         //cout<<n<<" "<<m;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(board[i][j]==word[0]){
-                    if(search(c,board,word,i,j,vis,0)){
-                        return true;
-                    }
+                if(search(c,board,word,i,j,vis,0)){
+                    return true;
                 }
             }
         }
