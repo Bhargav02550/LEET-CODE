616701b184efad817f57f39daaac0f5bab72c7b0
bhargav0255 <99163827+Bhargav02550@users.noreply.github.com>
2024-02-20 09:50:13 +0530

    Time: 19 ms (30.92%) | Memory: 20.4 MB (18.88%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -3,9 +3,7 @@ public:
     int missingNumber(vector<int>& nums) {
         int sum=0;
         int n = nums.size();
-        for(auto it:nums){
-            sum+=it;
-        }
+        sum = accumulate(nums.begin(),nums.end(),0);
         int osum = n*(n+1)/2;
         return osum-sum;
     }
