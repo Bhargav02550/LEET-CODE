fc38fdc501ae8ad0348805fbe2bdf8b2d59c7e31
bhargav0255 <99163827+Bhargav02550@users.noreply.github.com>
2024-02-22 18:48:30 +0530

    Time: 115 ms (91.60%) | Memory: 64.2 MB (73.94%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> trusts(n + 1, 0); // Array to store incoming trusts
+        vector<int> trusted(n + 1, 0);
+        for (const auto& relation : trust) {
+            int a = relation[0];
+            int b = relation[1];
+            trusts[b]++;
+            trusted[a]++;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (trusts[i] == n - 1 && trusted[i] == 0) {
+                return i;
+            }
+        }
+        return -1; 
+    }
+};
