1aa122badccba28b33548548114e1a8a317b3c7f
bhargav0255 <99163827+Bhargav02550@users.noreply.github.com>
2024-03-11 19:53:53 +0530

    Time: 0 ms (100.00%) | Memory: 23.3 MB (58.17%) - LeetSync

---
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        int count=0;
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]<target)count++;
+            }
+        }
+        return count;
+    }
+};
