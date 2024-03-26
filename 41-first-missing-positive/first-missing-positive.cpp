e157d96a0d59d8ab80ba328c8f13f9d27f356678
bhargav0255 <99163827+Bhargav02550@users.noreply.github.com>
2024-03-26 11:20:57 +0530

    Time: 28 ms (97.68%) | Memory: 44.3 MB (32.48%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& arr) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         sort(arr.begin(),arr.end());
         int ans=1;
         for(auto it:arr){
